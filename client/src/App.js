84615782181c4b3d51949e7dbdb802b60e611c3c
JohnDJake <john.d.jake@gmail.com>
2021-02-19 17:54:21 -0800

    generated react client app

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            {/* put everything here */}
+            <h1>title</h1>
+        </div>
+    )
+}
