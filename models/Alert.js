ab75289d69eac3daafcaa46c468b0d53fed833b1
JohnDJake <john.d.jake@gmail.com>
2021-02-23 21:30:58 -0800

    fixed alert cyclical dependency

--- models/Alert.js
+++ models/Alert.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     const Alert = sequelize.define("Alert", {
-        message: {type:DataTypes.STRING, allowNull: false},
+        message: { type: DataTypes.STRING, allowNull: false },
         read: {
             type: DataTypes.BOOLEAN,
             defaultValue: false
@@ -8,11 +8,11 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     Alert.associate = db => {
-        Alert.belongsTo(db.User);
-        db.User.hasMany(Alert, { foreignKey: { name: "to_user_id", allowNull: false } });
+        db.User.belongsTo(Alert, { foreignKey: { name: "to_user_id", allowNull: false } });
+        Alert.hasMany(db.User);
         Alert.hasOne(db.Dog, { foreignKey: { name: "about_dog_id", allowNull: true } });
         Alert.hasOne(db.User, { foreignKey: { name: "about_user_id", allowNull: true } });
-    };  
-    
+    };
+
     return Alert;
 };
