7e8ec90d17bf6ddeff702d952e47e6117e0493db
eubank87 <eubank.ashley@gmail.com>
2021-02-23 16:39:05 -0800

    added initial navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useState} from "react";
+import { ThemeProvider } from '@material-ui/styles';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
+import ResponsiveDrawer from "../src/components/Header" 
 
 export default function App() {
     return (
-        <div>
-            {/* put everything here */}
-        </div>
+        <ThemeProvider>
+            <BrowserRouter>
+                <ResponsiveDrawer/>
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
