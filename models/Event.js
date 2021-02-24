47b437a244600d539ad9d274e54519f2df33f78a
Shea Mullaney <seagda@gmail.com>
2021-02-23 20:24:13 -0800

    little fixes

--- models/Event.js
+++ models/Event.js
@@ -7,6 +7,12 @@ module.exports = (sequelize, DataTypes) => {
     Event.associate = db => {
         Event.hasOne(db.Address, {foreignKey: {allowNull: true}}
         );
+
+        Event.belongsToMany(db.Ext_Contact, { through: "EventsContacts" });
+        db.Ext_Contact.belongsToMany(Event, { through: "EventsContacts" });
+
+        Event.belongsToMany(db.User, { through: "EventsUsers" });
+        db.User.belongsToMany(Event, { through: "EventsUsers" });
     };  
     return Event;
 };
--- models/Ext_Contact.js
+++ models/Ext_Contact.js
@@ -1,10 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
     const Ext_Contact = sequelize.define("Ext_Contact", {
-
+        email: {
+            type: DataTypes.STRING,
+            validate: { isEmail: true },
+        },
+        phone: {type:DataTypes.STRING,allowNull:false},
+        full_name: {type:DataTypes.STRING, allowNull:false},
+        contact_type: {type:DataTypes.ENUM("shelter","rescue","vet","trainer","surrender","other")}
     });
 
     Ext_Contact.associate = db => {
-
+        Ext_Contact.hasOne(db.Address);
+        Ext_Contact.hasOne(db.Region);
     };
 
     return Ext_Contact;
