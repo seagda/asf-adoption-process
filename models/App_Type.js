6ff602ee167c39cdade82710ef1b09c339cdb47c
JohnDJake <john.d.jake@gmail.com>
2021-02-23 19:30:55 -0800

    set up app types and questions association

--- models/App_Type.js
+++ models/App_Type.js
@@ -1,8 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
     const App_Type = sequelize.define("App_Type", {
-        name: {type: DataTypes.STRING, allowNull: false} 
+        name: { type: DataTypes.STRING, allowNull: false }
 
-    })
+    });
+
+    App_Type.associate = db => {
+        App_Type.belongsToMany(db.App_Question, { through: "App_Types_Questions" });
+        db.App_Question.belongsToMany(App_Type, { through: "App_Types_Questions" });
+    };
+
     return App_Type;
 };
 
