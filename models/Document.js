634fd08bc2e0bf591966a057a256fc84a4830329
Shea Mullaney <seagda@gmail.com>
2021-02-23 21:14:50 -0800

    Document created

---
+++ models/Document.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+    const Document = sequelize.define("Document", {
+        url: {type:DataTypes.STRING, allowNull: false, validate: { isUrl: true }},
+        desc: {type:DataTypes.STRING, allowNull: false}
+    });
+
+    Document.associate = db => {
+        db.Dog.hasMany(Document);
+        Document.belongsTo(db.Dog);
+    };
+    return Document;
+};
+
+
+ 
