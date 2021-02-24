60cff373c7e4d4deb268731bf08a52c3c96af6ba
JohnDJake <john.d.jake@gmail.com>
2021-02-23 16:17:09 -0800

    created user model

---
+++ models/User.js
@@ -0,0 +1,67 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        create_key: DataTypes.STRING,
+        email: {
+            type: DataTypes.STRING,
+            validate: { isEmail: true },
+            allowNull: false,
+            uniqe: true
+        },
+        phone: DataTypes.STRING,
+        first_name: DataTypes.STRING,
+        last_name: DataTypes.STRING,
+        username: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        password: DataTypes.STRING,
+        dob: DataTypes.DATEONLY,
+        photo_url: {
+            type: DataTypes.STRING,
+            validate: { isUrl: true }
+        },
+        active: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true
+        },
+        admin_notes: DataTypes.STRING,
+        blocked: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        max_capacity: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        puppies: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        adults: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        seniors: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        with_behavior_issues: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        with_medical_issues: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        hold: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+
+    User.associate = db => {
+        User.hasOne(db.Address);
+        User.hasOne(db.Auth);
+    };
+};
+
