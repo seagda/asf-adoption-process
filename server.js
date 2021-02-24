1fe6696db81882b0bbf29d312892f40b2cfcdce9
JohnDJake <john.d.jake@gmail.com>
2021-02-23 21:03:56 -0800

    server syncs with db before starting

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const db = require("./models");
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,4 +20,4 @@ if (env === "production") {
     app.get((req, res) => res.sendFile(path.join(__dirname, "./client/build/index.html")));
 }
 
-app.listen(PORT, () => console.log(`API Server now listening on PORT ${PORT}`));
+db.sequelize.sync({ force: false }).then(() => app.listen(PORT, () => console.log(`API Server now listening on PORT ${PORT}`)));
