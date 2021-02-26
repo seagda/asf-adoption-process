1f73588d7cc0eef2ed3736dd71d9acf127f9b3c5
Shea Mullaney <seagda@gmail.com>
2021-02-26 15:01:15 -0800

    routes mapped out

--- controllers/dogController/index.js
+++ controllers/dogController/index.js
@@ -49,17 +49,118 @@ router.get("/", (req, res) => {
     } else return status(401).send({ message: "Not authorized to view dogs" });
 });
 
-// show one DOG, with correct ROLE permission
+// TODO: show one DOG, with correct ROLE permission
 
-// create new DOG, with correct ROLE permission
+router.get("/:id", (req, res) => {
 
-// update DOG by id, with correct ROLE permission
+});
 
-// delete DOG by id, with correct ROLE permission
+// TODO: create new DOG, with correct ROLE permission
 
-// read MEDI_STATUS with correct ROLE permission
+router.post("/new", (req, res) => {
+
+});
+
+// TODO: update own DOG by id, with correct ROLE permission
+
+router.put("/:id", (req, res) => {
+
+});
+
+// TODO: update any DOG by id, with correct ROLE permission (REGIONAL)
+
+router.put("/:id", (req, res) => {
+
+});
+
+// TODO: rename own DOG by id, update AliasTable with correct ROLE permission
+
+router.put("rename/:id", (req, res) => {
+
+});
+
+// TODO: rename any DOG by id, with correct ROLE permission
+
+router.put("rename/:id", (req, res) => {
+
+});
+
+// TODO: delete DOG by id, with correct ROLE permission
+
+router.delete("/archive/:id", (req, res) => {
+
+});
+
+
+// TODO: read all ASSESSMENTS for any DOG with correct ROLE permission
+
+router.get("/assess", (req, res) => {
+
+});
+
+// TODO: read all ASSESSMENTS for own DOG with correct ROLE permission
+
+router.get("/assess/:id", (req, res) => {
+
+});
+
+// TODO: create ASSESSMENT for DOG with correct ROLE permission
+
+router.get("/add-assessment", (req, res) => {
+
+});
+
+// TODO: update ASSESSMENT for DOG with correct ROLE permission
+
+router.put("/update-assessment/:id", (req, res) => {
+
+});
+
+// TODO: delete ASSESSMENT for DOG with correct ROLE permission
+
+router.delete("/archive-assessment/:id", (req, res) => {
+
+});
+
+// TODO: read all DOCUMENTS for any DOG with correct ROLE permission
+
+router.get("/documents", (req, res) => {
+
+});
+
+// TODO: read all DOCUMENTS for own DOG with correct ROLE permission
+
+router.get("/documents/:id", (req, res) => {
+
+});
+
+// TODO: create DOCUMENT for DOG with correct ROLE permission
+
+router.post("/add-document/:id", (req, res) => {
+
+});
+
+// TODO: update DOCUMENT for DOG with correct ROLE permission
+
+router.put("/update-document/:id", (req, res) => {
+
+});
+
+// TODO: delete DOCUMENT for DOG with correct ROLE permission
+
+router.delete("/archive-doc/:id", (req, res) => {
+
+});
+
+// TODO: create MEDI_STATUS for DOG with correct ROLE permission
+
+router.post("/medi-status", (req, res) => {
+
+});
+
+// read MEDI_STATUS for DOG with correct ROLE permission
 router.get("/medi-status/:id", (req, res) => {
-    const permission = ac.can(req.roles).updateOwn("Dog");
+    const permission = ac.can(req.roles).readOwn("Dog");
     if (permission.granted) {
         db.MediStatus
             .findOne({include: {model:db.Dog, where: {id:req.params.id}}})
@@ -71,15 +172,14 @@ router.get("/medi-status/:id", (req, res) => {
     } else return res.status(401).send({ message: "Not authorized to update a Medical Status"});
 });
 
-module.exports = router;
-// update MEDI_STATUS with correct ROLE permission
+// update MEDI_STATUS for DOG with correct ROLE permission
 
 router.put("/medi-status/:id", (req, res) => {
     const permission = ac.can(req.roles).updateOwn("Dog");
     if (permission.granted) {
         db.MediStatus
             .findOne({include: {model:db.Dog, where: {id:req.params.id}}})
-            .then(statusUp => statusUp.update(permission.filter(req.body)))
+            .then(medStat => res.json(permission.filter(medStat.toJSON())))
             .then(() => res.sendStatus(200))
             .catch(err => {
                 console.error(err);
@@ -89,18 +189,3 @@ router.put("/medi-status/:id", (req, res) => {
 });
 
 module.exports = router;
-
-/*
-TODO: Here are the routes to create for dog
-
-/add-document
-/add-event
-/archive
-/assess
-/assign
-/edit-detail
-/edit-contact
-
-/rename
-
-*/
