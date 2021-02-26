a602b3596fa408960097ee830757afd6511beddc
JohnDJake <john.d.jake@gmail.com>
2021-02-26 10:08:03 -0800

    questions inside categories

--- controllers/appQuestionController.js
+++ controllers/appQuestionController.js
@@ -9,10 +9,12 @@ router.get("/:typeId", (req, res) => {
     const permission = ac.can(req.roles).readAny("AppQuestion");
     if (permission.granted) {
         // TODO: findall appquestioncategory include appquestion which will include apptype where id is typeid, then map to question panels in surveyjs format
-        db.AppQuestion.findAll({ include: [db.AppQuestionCategory, { model: db.AppType, where: { id: req.params.typeId } }] }).then(questions => {
+        /* db.AppQuestion.findAll({ include: [db.AppQuestionCategory, { model: db.AppType, where: { id: req.params.typeId } }] }).then(questions => {
             console.log(questions);
             res.send(questions);
-        });
+        }); */
+        const id = req.params.typeId;
+        db.AppQuestionCategory.findAll({ include: { model: db.AppQuestion, include: { model: db.AppType, where: { id } } } }).then(categories => res.json(categories));
     } else res.status(401).send({ message: "Not authorized to view application questions" });
 });
 
