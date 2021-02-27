0d2482d833385153d2c22e448f62240d75dafa2a
JohnDJake <john.d.jake@gmail.com>
2021-02-27 15:48:18 -0800

    get app questions by type

--- controllers/appQuestionController.js
+++ controllers/appQuestionController.js
@@ -8,13 +8,43 @@ const router = require("express").Router();
 router.get("/:typeId", (req, res) => {
     const permission = ac.can(req.roles).readAny("AppQuestion");
     if (permission.granted) {
-        // TODO: findall appquestioncategory include appquestion which will include apptype where id is typeid, then map to question panels in surveyjs format
-        /* db.AppQuestion.findAll({ include: [db.AppQuestionCategory, { model: db.AppType, where: { id: req.params.typeId } }] }).then(questions => {
-            console.log(questions);
-            res.send(questions);
-        }); */
-        const id = req.params.typeId;
-        db.AppQuestionCategory.findAll({ include: { model: db.AppQuestion, include: { model: db.AppType, where: { id } } } }).then(categories => res.json(categories));
+        db.AppQuestionCategory.findAll({
+            include: {
+                model: db.AppQuestion,
+                include: [
+                    db.AppQuestionOption,
+                    { model: db.AppQuestion, as: "DependsOnQuestion", include: db.AppQuestionOption },
+                    { model: db.AppType, where: { id: req.params.typeId } }]
+            }
+        })
+            .then(categories => {
+                const panels = categories.map(category => ({
+                    title: category.name,
+                    type: "panel",
+                    name: `panel${category.id}`,
+                    questions: category.AppQuestions.map(appQuestion => {
+                        const question = (({ name, title, type, isRequired }) => ({ name, title, type, isRequired }))(appQuestion);
+                        // number and url types are actually subtypes of text
+                        if (question.type === "number" || question.type === "url") {
+                            question.inputType = question.type;
+                            question.type = "text";
+                        } else if (question.type === "radiogroup") question.choices = appQuestion.AppQuestionOptions.map(option => option.option);
+                        if (appQuestion.DependsOnQuestion) {
+                            question.visibleIf = `{${appQuestion.DependsOnQuestion.name}}=`
+                            if (appQuestion.DependsOnQuestion.type === "boolean")
+                                question.visibleIf += appQuestion.dependsOnAnswer;
+                            else if (appQuestion.DependsOnQuestion.type === "radiogroup")
+                                question.visibleIf += `'${appQuestion.DependsOnQuestion.AppQuestionOptions.find(option => option.id === appQuestion.dependsOnAnswer).option}'`
+                        }
+                        return question;
+                    })
+                }));
+                res.json(panels);
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send({ message: "Database access error" });
+            });
     } else res.status(401).send({ message: "Not authorized to view application questions" });
 });
 
