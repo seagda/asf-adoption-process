7b6efca910f839eb7558dff25a592f69743db56e
JohnDJake <john.d.jake@gmail.com>
2021-03-01 10:39:42 -0800

    role name must be unique

--- models/Role.js
+++ models/Role.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     const Role = sequelize.define("Role", {
-        name: { type: DataTypes.STRING, allowNull: false }
+        name: { type: DataTypes.STRING, allowNull: false, unique: true }
     }, { paranoid: false });
     return Role;
 };
