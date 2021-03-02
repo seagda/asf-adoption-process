4b8d0f8b70c05bd2f6576e17df5a9354c3e33c61
JohnDJake <john.d.jake@gmail.com>
2021-03-01 17:53:05 -0800

    get dog counts by status

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,4 +2,21 @@ const db = require("../models");
 const ac = require("../helpers/ac");
 const router = require("express").Router();
 
+const STATIC_IDS = require("../scripts/staticIds");
+
+// get all info for dashboard
+router.get("/", (req, res) => {
+    const dashboardPromises = [];
+    const permissionAnyDog = ac.can(req.roles).readAny("Dog");
+    if (permissionAnyDog.granted) {
+        dashboardPromises[0] = db.Dog.count({ include: db.DogStatus, group: ["DogStatusId"] });
+    }
+    Promise.all(dashboardPromises)
+        .then(([DogStatusCounts]) => {
+            console.log(DogStatusCounts);
+            res.json({DogStatusCounts});
+        })
+        .catch(console.error);
+});
+
 module.exports = router;
