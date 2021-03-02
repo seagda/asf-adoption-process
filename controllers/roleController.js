5d06377c7666035223fcc0fcf7ffdf0cefdcdf5a
JohnDJake <john.d.jake@gmail.com>
2021-03-02 13:29:29 -0800

    roles controller sends all roles

---
+++ controllers/roleController.js
@@ -0,0 +1,9 @@
+const db = require("../models");
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+    db.Role.findAll().then(roles => res.json(roles)).catch(err => {
+        console.error(err);
+        res.status(500).send({ message: "something is broken" });
+    });
+});
