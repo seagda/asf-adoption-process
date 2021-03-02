faa6c6f07603a5ad545880a878a1f824568c9289
JohnDJake <john.d.jake@gmail.com>
2021-03-01 18:36:33 -0800

    dashboard route includes pending apps and user's own dogs

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -11,10 +11,31 @@ router.get("/", (req, res) => {
     if (permissionAnyDog.granted) {
         dashboardPromises[0] = db.Dog.count({ include: db.DogStatus, group: ["DogStatusId", "DogStatus.name"] });
     }
+    const permissionAnyAppResponse = ac.can(req.roles).readAny("AppResponse");
+    if (permissionAnyAppResponse.granted) {
+        dashboardPromises[1] = db.AppResponse.count({ where: { AppStatusId: { [db.Sequelize.Op.lt]: STATIC_IDS.APP_STATUS.APPROVED } }, include: db.AppType, group: ["AppTypeId", "AppType.name"] });
+
+        // none of this works the way I want it to, if at all:
+        /* // dashboardPromises[1]
+        db.AppType.findAll({ include: { model: db.AppResponse, where: { AppStatusId: STATIC_IDS.APP_STATUS.APPROVED }, include: db.User } }).then()
+        // db.AppResponse.findAll({ where: { AppStatusId: STATIC_IDS.APP_STATUS.APPROVED }, include: db.User }).then(appResponses => {
+        //     appResponses.forEach(appResponse => appResponse.User.getAvailableCapacity())
+        // });
+        //     // dashboardPromises[1] = db.AppResponse.findAll({include: [db.AppStatus]})
+        //     db.User.findByPk(3).then(user => user.getDogsByStatus("Adopted")).then(console.log) */
+    }
+    const permissionOwnDog = ac.can(req.roles).readOwn("Dog");
+    if (permissionOwnDog.granted) {
+        dashboardPromises[2] = db.Dog.findAll({ where: { currentlyWithId: req.userId }, order: ["DogStatusId"] });
+    }
+
     Promise.all(dashboardPromises)
-        .then(([DogStatusCounts]) => {
-            console.log(DogStatusCounts);
-            res.json({ DogStatusCounts });
+        .then(([dogStatusCounts, pendingAppCounts, myDogs]) => {
+            res.json({
+                dogStatusCounts: permissionAnyDog.filter(dogStatusCounts),
+                pendingAppCounts: permissionAnyAppResponse.filter(pendingAppCounts),
+                myDogs: permissionOwnDog.filter(myDogs)
+            });
         })
         .catch(console.error);
 });
