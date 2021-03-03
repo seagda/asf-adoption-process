e9faab7286ca21ea7ad3eea031298570e84eeeb3
HannahStarcevich <starcevichh@gmail.com>
2021-03-03 11:05:56 -0800

    updates to dashboard conditional data displays

--- client/src/pages/DashboardMain.js
+++ client/src/pages/DashboardMain.js
@@ -47,7 +47,6 @@ export default function DashboardMain(){
     const [dashboardData, setDashboardState] = useState({})
   
     function loadDashboard() {
-        
     API.getDashboardData()
         .then(res => {
         console.log(res)
@@ -76,69 +75,87 @@ export default function DashboardMain(){
                     Quick Actions
                     <Divider />
                 </Typography>
+
+                {/* If Admin credentials, display these quick actions */}
                 {JSON.parse(localStorage.getItem("user")).roles.some( (role) => ["regional", "admin", "superAdmin"].includes(role)) ? 
                 <Grid item xs={12}>
                     <QuickActionsAdmin/>
                 </Grid>: null}
+
+                {/* And/or if Foster credentials, display these quick actions */}
                 {JSON.parse(localStorage.getItem("user")).roles.includes("foster") ?
                 <Grid item xs={12}>
                     <QuickActionsFoster/>
                 </Grid>: null}
             </Grid>
-            {dashboardData.fosterCounts && dashboardData.adopterCounts && dashboardData.dogStatusCounts ? <Grid item xs={12}>
-                <PieChartContainer data={dashboardData}/>
-            </Grid>: null}
-            {JSON.parse(localStorage.getItem("user")).roles.includes("foster") ?
+
+            <Grid item xs={12} s={10}>
+
+                {/* Render pie charts for admin viewers */}
+                {dashboardData.fosterCounts && dashboardData.adopterCounts && dashboardData.dogStatusCounts ? 
+                <Grid item xs={12}>
+                    <PieChartContainer data={dashboardData}/>
+                </Grid>: null}
+
+                {/* Render dog photo cards if user has associated dogs */}
+                
+                {dashboardData.myDogs && dashboardData.myDogs.length ? (
+                <React.Fragment>
                 <Grid item xs={12} s={10}>
                     <Typography variant="h5" component="h6" gutterBottom color="primary">
                         My Dogs
                         <Divider />
                     </Typography>
-                </Grid>:null}
-            {dashboardData.myDogs.length ? (
-                <Grid container className={classes.cardContainer} justify="center">
-                        {dashboardData.myDogs.map(dog =>{
-                            return (
-                                <Grid item xs={10} s={10} m={6} lg={3}>
-                                    <MediaCard name={dog.name} /* image={dog.image} */ />
-                                </Grid>
-                            )
-                        })}    
                 </Grid>
-                ):(<p>Currently no data to display</p>)} 
-        
-            
-           
-            {/* <Grid item xs={12} s={12} m={6} lg={6} style={{marginTop: "2em"}}>
-                 <Typography variant="h5" component="h4" gutterBottom align="center" color="primary">
-                    ASF Team Members
-                    <Divider />
-                </Typography>
-                {team.length ? (
-                <ListContainer>
-                    {team.map(teamMember =>{
-                        return (
-                            <AvatarList name={teamMember.name} image={teamMember.image} role={teamMember.role} city={teamMember.city} email={teamMember.email}/>
-                        )
-                    })}    
-                </ListContainer>
-                ):(<p>Currently no data to display</p>)}
+                    <Grid container className={classes.cardContainer} justify="center">
+                            {dashboardData.myDogs.map(dog =>{
+                                return (
+                                    <Grid item xs={10} s={10} m={6} lg={3}>
+                                        <MediaCard name={dog.name} />
+                                    </Grid>
+                                )
+                            })}    
+                    </Grid>
+                </React.Fragment>
+                    ):null} 
+
+                {/* Render ASF Regional Leads and team if user is admin */}
+                {/* {dashboardData.teamArray ? 
+                <Grid item xs={12} s={12} m={6} lg={6} style={{marginTop: "2em"}}>
+                    <Typography variant="h5" component="h4" gutterBottom align="center" color="primary">
+                        ASF Team Members
+                        <Divider />
+                    </Typography> 
+                    
+                    {dashboardData.teamArray.length ? (
+                        <ListContainer>
+                            {dashboardData.teamArray.map(teamMember =>{
+                                return (
+                                    <AvatarList firstName={teamMember.firstName} lastName={teamMember.lastName} image={teamMember.photoUrl} roles={teamMember.Roles} city={teamMember.city} email={teamMember.email}/>
+                                )
+                            })}    
+                        </ListContainer>
+                    ):null}
+                <Grid/>:null} */}
+
+                {/* Render user alerts */}
+                {dashboardData.alerts ? (
+                <Grid item xs={12} s={12} m={6} lg={6} style={{marginTop: "2em"}}>
+                    <Typography variant="h5" component="h4" gutterBottom align="center" color="primary">
+                        My Alerts
+                        <Divider />
+                    </Typography>
+                    {dashboardData.alerts.length ? (
+                        <ListContainer>
+                            {dashboardData.alerts.map(alert =>{
+                                return (
+                                    <BasicList name={alert.name} message={alert.message} />
+                                )
+                            })}    
+                        </ListContainer>
+                    ):(<p>You are all caught up!</p>)}
+                </Grid>) :null}
             </Grid>
-            <Grid item xs={12} s={12} m={6} lg={6} style={{marginTop: "2em"}}>
-                <Typography variant="h5" component="h4" gutterBottom align="center" color="primary">
-                    My Alerts
-                    <Divider />
-                </Typography>
-                {alerts.length ? (
-                <ListContainer>
-                    {alerts.map(alert =>{
-                        return (
-                            <BasicList name={alert.name} image={alert.image} dueDate={alert.dueDate} dogName={alert.dogName}/>
-                        )
-                    })}    
-                </ListContainer>
-                ):(<p>Currently no data to display</p>)}
-            </Grid> */}
         </Grid>
     )
 }
