1e083fb81c9fd383fd6c63eae2a3b1f1eb9c0036
JohnDJake <john.d.jake@gmail.com>
2021-03-08 19:09:59 -0800

    reference info can't be null

--- models/Reference.js
+++ models/Reference.js
@@ -1,12 +1,18 @@
 module.exports = (sequelize, DataTypes) => {
     const Reference = sequelize.define("Reference", {
-        fullName: DataTypes.STRING,
+        fullName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         email: {
             type: DataTypes.STRING,
             validate: { isEmail: true },
             allowNull: false
         },
-        phone: DataTypes.STRING,
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         contacted: {
             type: DataTypes.BOOLEAN,
             defaultValue: false
@@ -18,7 +24,6 @@ module.exports = (sequelize, DataTypes) => {
     Reference.associate = db => {
         Reference.belongsTo(db.User);
         db.User.hasMany(Reference);
-
     };
 
     return Reference;
