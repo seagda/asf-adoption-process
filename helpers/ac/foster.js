8c2fd0fe1358b39c0571109d2ffef96201a0f5e3
JohnDJake <john.d.jake@gmail.com>
2021-03-09 20:27:10 -0800

    missing brackets

--- helpers/ac/foster.js
+++ helpers/ac/foster.js
@@ -1,6 +1,6 @@
 module.exports = ac => {
     ac.grant("Adopter").grant("Foster").extend("Adopter")
-        .readAny("User", "!blocked", "!adminNotes")
+        .readAny("User", ["!blocked", "!adminNotes"])
         .updateOwn("Dog", ["DogStatus", "DogStatusId"])
         .readAny("AppResponseAdopter")
         .createAny("BehavorialAssessment")
