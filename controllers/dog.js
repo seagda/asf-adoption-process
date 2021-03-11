dedbae22cebcec1902bd5f8bca1a8db48e9b02d6
JohnDJake <john.d.jake@gmail.com>
2021-03-11 15:00:24 -0800

    dog controller set up with update functions

---
+++ controllers/dog.js
@@ -0,0 +1,16 @@
+// const db = require("../models");
+const STATIC_IDS = require("../scripts/staticIds");
+const alertController = require("./alert");
+
+/* module.exports.getAll = () => {
+
+} */
+
+module.exports.update = (dog, updates) => { };
+
+module.exports.updateCurrentlyWith = (dog, CurrentlyWithId) => { };
+
+module.exports.updateStatus = (dog, DogStatusId) => Promise.resolve(dog.DogStatusId == DogStatusId ? false : Promise.all([dog.update({ DogStatusId }), dog.getRegion()]))
+.then(updated => updated ? alertController.dogStatus(updated[0], updated[1]) : false)
+
+module.exports.updateName = (dog, name) => { };
