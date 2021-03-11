511f8268fcfd57a810182f76a237fb6282ae3ddf
JohnDJake <john.d.jake@gmail.com>
2021-03-11 15:00:01 -0800

    alert controller set up to get users for dog status alerts

--- controllers/alert.js
+++ controllers/alert.js
@@ -1,7 +1,8 @@
 const db = require("../models");
 const mail = require("../helpers/mail");
+const STATIC_IDS = require("../scripts/staticIds");
 
-module.exports.notifyUsers = alerts => Promise.all(alerts.map(alert => alert.getToUser({ include: db.Setting })))
+/* module.exports.notifyUsers = alerts => Promise.all(alerts.map(alert => alert.getToUser({ include: db.Setting })))
     .then(users => {
         for (let i = 0; i < alerts.length; i++) {
             if (users[i].Setting.email) mail.sendMail({
@@ -13,4 +14,19 @@ module.exports.notifyUsers = alerts => Promise.all(alerts.map(alert => alert.get
             })
         }
     })
-    .catch(console.error)
+    .catch(console.error) */
+
+module.exports.dogStatus = (dog, region) => {
+    const or = [
+        { "$Roles.id$": STATIC_IDS.ROLES.SUPERADMIN },
+        { [db.Sequelize.Op.and]: [{ "$AssignedRegions.id$": region.id }, { "$Roles.id$": STATIC_IDS.ROLES.REGIONAL }] }
+    ];
+    return Promise.all([
+        db.User.findAll({
+            where: {
+                [db.Sequelize.Op.or]: or
+            }, include: [db.Role, { association: "AssignedRegions" }]
+        }),
+        dog.getDogStatus()
+    ]).then(([users, dogStatus]) => console.log(users))
+}
