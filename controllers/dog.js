0117067eb61a3356a96c6bc0907207e2c6057421
JohnDJake <john.d.jake@gmail.com>
2021-03-11 17:05:32 -0800

    indent

--- controllers/dog.js
+++ controllers/dog.js
@@ -11,6 +11,6 @@ module.exports.update = (dog, updates) => { };
 module.exports.updateCurrentlyWith = (dog, CurrentlyWithId) => { };
 
 module.exports.updateStatus = (dog, DogStatusId) => Promise.resolve(dog.DogStatusId == DogStatusId ? false : Promise.all([dog.update({ DogStatusId }), dog.getRegion()]))
-.then(updated => updated ? alertController.dogStatus(updated[0], updated[1]) : false)
+    .then(updated => updated ? alertController.dogStatus(updated[0], updated[1]) : false);
 
 module.exports.updateName = (dog, name) => { };
