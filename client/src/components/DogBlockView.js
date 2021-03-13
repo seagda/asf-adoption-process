55af2cc6b39016e0656ee37c4d8114217f549f32
eubank87 <eubank.ashley@gmail.com>
2021-03-13 12:31:14 -0800

    minor styling adjustment for mobile view

--- client/src/components/DogBlockView.js
+++ client/src/components/DogBlockView.js
@@ -26,6 +26,7 @@ const useStyles = makeStyles(theme => ({
     picContainer: {
         [theme.breakpoints.down("xs")]:{
             justifyContent: "center",
+            padding: 5
         }
     }
 }))
