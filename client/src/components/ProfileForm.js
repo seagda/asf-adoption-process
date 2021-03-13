fa19ffeacb80a2a083aeda214012aeeeb89d1a05
eubank87 <eubank.ashley@gmail.com>
2021-03-13 10:28:00 -0800

    removed hard coded capacity section from profile form

--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -248,34 +248,6 @@ export default function ProfileForm(props){
     // </Grid>
     // )
 
-    const capacity = (
-        <Grid item container className={classes.itemContainer, classes.marginFix}>
-            <Grid container direction="row" justify={"space-between"}>
-            <Grid item container xs={10} sm={6} md={6} lg={6} style={{marginTop: "3em"}} direction="column">
-                <Grid item>
-                    <Typography variant="h4">Capacity</Typography>
-                    <Divider/>
-                </Grid>
-                <Grid item container>
-                    <Grid item xs={6} sm={6} md={8} lg={6}>
-                        <Typography style={{marginTop: "1em"}}>Max Capacity:</Typography>
-                        {/* <Typography style={{marginTop: "1.5em"}}>Dogs in care:</Typography>
-                        <Typography style={{marginTop: "2em"}}>Available space:</Typography> */}
-                    </Grid>
-                    <Grid item xs={6} sm={6} md={8} lg={6}>
-                        <div>
-                        <TextField variant="outlined" type="number" style={{marginTop: "0.5em"}} onChange={createUserInputChange} value={userIntakeData.maxCapacity} name="maxCapacity"></TextField>
-                        {/* <TextField type="number" style={{marginTop: "1em"}}></TextField>
-                        <TextField type="number" style={{marginTop: "1em"}}></TextField> */}
-                        </div>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
-        </Grid>
-
-    )
-
     const caresFor = (
         <Grid item container className={classes.itemContainer}>
             <Grid item container xs={10} sm={6} md={6} lg={6} style={{marginTop: "3em"}} direction="column">
@@ -513,7 +485,6 @@ export default function ProfileForm(props){
             />
 
         </Grid>
-        {/* {capacity} */}
 
         <UserCapacityEdit
         createUserChange={createUserInputChange} 
