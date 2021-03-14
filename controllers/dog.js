9fb53acc7f6d5ad5ad0377e10fcb08f340066dcc
JohnDJake <john.d.jake@gmail.com>
2021-03-14 14:04:29 -0700

    get all dogs with filters

--- controllers/dog.js
+++ controllers/dog.js
@@ -1,10 +1,24 @@
-// const db = require("../models");
-const STATIC_IDS = require("../scripts/staticIds");
+const db = require("../models");
 const alertController = require("./alert");
 
-/* module.exports.getAll = () => {
-
-} */
+module.exports.getAll = (originFilter, withFilter, regionFilter) => db.Dog.findAll({
+    include: [
+        db.DogStatus,
+        { association: "Origin", where: originFilter, include: [db.Address, { model: db.Region, where: regionFilter }] },
+        { association: "CurrentlyWith", where: withFilter, include: [db.Address, { association: "ResidesInRegion", where: regionFilter }] },
+        { model: db.DogPhoto, required: false, where: { profilePhoto: true } }
+    ]
+}).then(dogs => dogs.map(dog => {
+    const dogJson = dog.toJSON();
+    if (dogJson.CurrentlyWithId) {
+        dogJson.Address = dogJson.CurrentlyWith.Address;
+        dogJson.Region = dogJson.CurrentlyWith.ResidesInRegion;
+    } else {
+        dogJson.Address = dogJson.Origin.Address;
+        dogJson.Region = dogJson.Origin.Region;
+    }
+    return dogJson;
+}));
 
 module.exports.update = (dog, updates) => { };
 
