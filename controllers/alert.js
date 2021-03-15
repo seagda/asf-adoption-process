26a41a477cba692a0590fc97d0194609ab02180e
JohnDJake <john.d.jake@gmail.com>
2021-03-14 17:43:41 -0700

    get alerts

--- controllers/alert.js
+++ controllers/alert.js
@@ -2,19 +2,11 @@ const db = require("../models");
 const mail = require("../helpers/mail");
 const STATIC_IDS = require("../scripts/staticIds");
 
-/* module.exports.notifyUsers = alerts => Promise.all(alerts.map(alert => alert.getToUser({ include: db.Setting })))
-    .then(users => {
-        for (let i = 0; i < alerts.length; i++) {
-            if (users[i].Setting.email) mail.sendMail({
-                from: '"Australian Shepherds Furever" <aussiesfurever@outlook.com>',
-                to: users[i].email,
-                subject: alerts[i].message,
-                text: alerts[i].message,
-                html: alerts[i].message
-            })
-        }
-    })
-    .catch(console.error) */
+module.exports.getAll = (filter) => db.Alert.findAll({ where: filter });
+
+module.exports.getUnread = (filter) => db.Alert.findAll({ where: { ...filter, read: false } });
+
+module.exports.getRead = (filter) => db.Alert.findAll({ where: { ...filter, read: true } });
 
 module.exports.dogStatus = (dog, region) => {
     const or = [
