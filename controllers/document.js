ec2168a2c48ccd667a0292fab0c5dabf80d2ea3d
JohnDJake <john.d.jake@gmail.com>
2021-03-15 15:15:46 -0700

    changed get docs for dog return format

--- controllers/document.js
+++ controllers/document.js
@@ -1,4 +1,4 @@
 const db = require("../models");
 
 module.exports.getForDog = (id) => db.Dog.findByPk(id, { include: db.Document })
-    .then(dog => dog.toJSON().Documents.map(document => ({ ...document, CurrentlyWithId: dog.CurrentlyWithId })));
+    .then(dog => ({ documents: dog.toJSON().Documents, CurrentlyWithId: dog.CurrentlyWithId }));
