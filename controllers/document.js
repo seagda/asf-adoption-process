cb184b79d8b862b8e32fa1594bd7b6616bb60a1d
JohnDJake <john.d.jake@gmail.com>
2021-03-17 17:08:39 -0700

    function to upload file and add to documents table

--- controllers/document.js
+++ controllers/document.js
@@ -10,5 +10,14 @@ module.exports.get = (id) => db.Document.findByPk(id, { include: db.Dog })
         return Promise.all([doc, file, file.getMetadata()]);
     });
 
+module.exports.create = (DogId, file) => {
+    const gcsFile = docsBucket.file(`${DogId}/${file.name}`);
+    return Promise.all([gcsFile.exists(), db.Document.findOne({ where: { DogId, name: file.name } })]).then(([exists, doc]) => {
+        if (exists[0] && doc) throw new Error("File already exists");
+        else if (doc) return doc;
+        return gcsFile.save(file.data);
+    }).then(doc => doc || db.Document.create({ DogId, name: file.name }));
+};
+
 module.exports.getForDog = (id) => db.Dog.findByPk(id, { include: db.Document })
     .then(dog => ({ documents: dog.toJSON().Documents, CurrentlyWithId: dog.CurrentlyWithId }));
