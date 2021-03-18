cccf6fbf5d824b64917a4b4fdd7313073367efa0
JohnDJake <john.d.jake@gmail.com>
2021-03-17 17:05:24 -0700

    document name must be unique to dog

--- models/Document.js
+++ models/Document.js
@@ -1,10 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
     const Document = sequelize.define("Document", {
-        name: { type: DataTypes.STRING, allowNull: false }
+        name: { type: DataTypes.STRING, allowNull: false, unique: "name" }
     });
 
     Document.associate = db => {
-        db.Dog.hasMany(Document, { foreignKey: { allowNull: false } });
+        db.Dog.hasMany(Document, { foreignKey: { allowNull: false, unique: "name" } });
         Document.belongsTo(db.Dog);
     };
     return Document;
