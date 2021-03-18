f6af6ca39930db75c55e2a990ab3628fc8bf3b86
JohnDJake <john.d.jake@gmail.com>
2021-03-17 17:08:58 -0700

    updated route to create documents for a dog

--- routes/dog/document.js
+++ routes/dog/document.js
@@ -22,31 +22,25 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// create new DOCUMENT, with correct ROLE permission
-router.post("/:id", (req, res) => {
+// create new DOCUMENTs, with correct ROLE permission
+router.post("/:DogId", require("express-fileupload")(), (req, res) => {
     const permissionOwn = ac.can(req.roles).createOwn("Document");
     const permissionAny = ac.can(req.roles).createAny("Document");
-    db.Dog.findByPk(req.params.id)
-    .then(dog => {
-        if (permissionOwn.granted || permissionAny.granted) {
-            let docJson;
-            if (dog.currentlyWithId === req.userId) {
-                docJson = permissionOwn.filter(req.body)
-            } else if (permissionAny.granted) {
-                docJson = permissionAny.filter(req.body)
-            } else return res.status(403).send({ message: "you can't create assessments" })
-    
-        dog
-          .createDocument(docJson) 
-          .then(() => res.status(200).send({message: "DOCUMENT successfully created"}))
-          .catch(err => {
-            console.error(err)  
-            res.status(422).send({ message: "Error with request" })
+    Promise.resolve(permissionAny.granted || (permissionOwn.granted && db.Dog.findByPk(req.params.DogId).then(dog => dog.CurrentlyWithId === req.userId)))
+        .then(granted => granted ?
+            Promise.allSettled(Object.values(req.files).map(file => controllers.document.create(req.params.DogId, file)))
+                .then(results => {
+                    if (results.filter(result => result.status === "rejected").some(result => result.reason.message !== "File already exists")) {
+                        console.error(results);
+                        res.sendStatus(500);
+                    }
+                    res.send(results.filter(result => result.status === "fulfilled").map(result => result.value));
+                }) :
+            res.status(403).send({ message: "Not authorized to add documents to this dog" }))
+        .catch(err => {
+            console.error(err);
+            res.sendStatus(500);
         });
-    
-        } else return res.status(403).send({ message: "Not authorized to create DOCUMENTs" })
-
-    })
 });
 
 // update DOCUMENT by id, with correct ROLE permission
@@ -54,9 +48,9 @@ router.put("/:id", (req, res) => {
     const permissionOwn = ac.can(req.roles).updateOwn("Document");
     const permissionAny = ac.can(req.roles).updateAny("Document");
     if (permissionOwn.granted || permissionAny.granted) {
-        
+
         db.Document
-        .findByPk(req.params.id, {include: db.Dog})
+            .findByPk(req.params.id, { include: db.Dog })
             .then(doc => {
                 let docJson;
                 if (doc.Dog.currentlyWithId === req.userId) {
@@ -71,7 +65,7 @@ router.put("/:id", (req, res) => {
                 console.error(err);
                 res.status(422).send({ message: "Error with request" })
             });
-    } else return res.status(403).send({ message: "Not authorized to update DOCUMENTs"});
+    } else return res.status(403).send({ message: "Not authorized to update DOCUMENTs" });
 });
 
 // delete DOCUMENT by id, with SuperAdmin ONLY permission
@@ -79,15 +73,16 @@ router.delete("/:id", (req, res) => {
     const permission = ac.can(req.roles).deleteAny("Document");
     if (permission.granted) {
         db.Document
-            .destroy({ where: {id: req.params.id} })
+            .destroy({ where: { id: req.params.id } })
             .then(deletedDoc => {
                 res.sendStatus(200);
-                console.log(`Assessment successfully deleted? 1 means yes, 0 means no: ${deletedDoc}`)})
+                console.log(`Assessment successfully deleted? 1 means yes, 0 means no: ${deletedDoc}`)
+            })
             .catch(err => {
                 console.error(err);
-              res.status(422).send({ message: "Error with request" })
-        });
-    } else return res.status(403).send({ message: "Not authorized to delete DOCUMENTs"});
+                res.status(422).send({ message: "Error with request" })
+            });
+    } else return res.status(403).send({ message: "Not authorized to delete DOCUMENTs" });
 });
 
 
