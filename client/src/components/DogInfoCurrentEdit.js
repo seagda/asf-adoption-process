647730be6120506a9bf447490dd9785b557291a6
eubank87 <eubank.ashley@gmail.com>
2021-03-19 16:05:07 -0700

    added multiselect for asf team memebr drop down

--- client/src/components/DogInfoCurrentEdit.js
+++ client/src/components/DogInfoCurrentEdit.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
 import {NavLink} from "react-router-dom";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
@@ -28,11 +32,27 @@ export default function DogInfoCurrentEdit(props){
                     <Grid item container style={{marginTop: "1em"}}>
                         <Grid item xs={10} sm={6} md={8} lg={6}>
                             <Typography style={{fontWeight: "bold"}}>Currently With:
-                                
+
                             </Typography> 
                         </Grid>
-                        <Grid item xs={10} sm={6} md={8} lg={6}>
-
+                        <Grid item style={{marginTop: "1em"}}>
+                            <div>
+                                <FormControl variant="outlined" style={{minWidth: 195, marginBottom: "1em"}}>
+                                    <InputLabel id="demo-simple-select-outlined-label">Microchip Company</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-outlined-label"
+                                        id="demo-simple-select-outlined"
+                                        // InputLabelProps={{shrink: true}}
+                                        name="MicrochipMfgId"
+                                        // value={selectData.MicrochipMfgId}
+                                        // onChange={handleSelectChange}
+                                    >
+                                        <MenuItem disabled value="">Microchip Manufacturer</MenuItem>
+                                        {props.mfgList}
+                                        
+                                    </Select>
+                                </FormControl>
+                            </div>
                         </Grid>
                         <Grid item xs={10} sm={6} md={8} lg={6}>
                             
