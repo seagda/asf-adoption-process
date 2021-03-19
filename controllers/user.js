6ad58ac9d05787e9560584fcd82163cda6f60655
JohnDJake <john.d.jake@gmail.com>
2021-03-18 18:35:55 -0700

    get profile photo from cloud storage

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,16 @@
 const db = require("../models");
 
+const storage = new (require("@google-cloud/storage")).Storage();
+const photosBucket = storage.bucket("dog-dossier-photos");
+
 module.exports.get = (id) => db.User.findByPk(id, {
     include: [
         db.Role, db.Address,
         { association: "ResidesInRegion" }, { association: "AssignedRegions" }
     ]
 }).then(user => user.toJSON());
+
+module.exports.getProfilePhoto = (id) => {
+    const photo = photosBucket.file(`user/${id}`);
+    return Promise.all([photo, photo.getMetadata()]);
+};
