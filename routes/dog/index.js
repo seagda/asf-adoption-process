229fd2f46dcbc233d6d205b036937bf1730875ca
JohnDJake <john.d.jake@gmail.com>
2021-03-19 00:48:17 -0700

    get less from database instead of ignoring it later

--- routes/dog/index.js
+++ routes/dog/index.js
@@ -54,8 +54,8 @@ router.get("/:DogId/assessments", (req, res) => {
     const permissionOwn = ac.can(req.roles).readOwn("BehavioralAssessment");
     const permissionAny = ac.can(req.roles).readAny("BehavioralAssessment");
     if (permissionOwn.granted || permissionAny.granted) {
-        db.Dog.findByPk(req.params.DogId, { include: { model: db.BehavioralAssessment, include: db.User } }).then(dog => {
-            let behAssJson = dog.BehavioralAssessments.map(behAss => ({ ...behAss.toJSON(), User: { firstName: behAss.User.firstName, lastName: behAss.User.lastName } }));
+        db.Dog.findByPk(req.params.DogId, { include: { model: db.BehavioralAssessment, include: {model: db.User, attributes: ["firstName", "lastName"]} } }).then(dog => {
+            let behAssJson = dog.BehavioralAssessments.map(behAss => behAss.toJSON());
             if (dog.CurrentlyWithId === req.userId) {
                 behAssJson = permissionOwn.filter(behAssJson)
             } else if (permissionAny.granted) {
