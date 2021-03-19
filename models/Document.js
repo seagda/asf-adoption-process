7094901342c0df63ea2cacb2189edde786655610
JohnDJake <john.d.jake@gmail.com>
2021-03-18 17:28:20 -0700

    document model is not paranoid
    deleting a document deletes the actual file, no point in keeping the
    archived record

--- models/Document.js
+++ models/Document.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
     const Document = sequelize.define("Document", {
         name: { type: DataTypes.STRING, allowNull: false, unique: "name" }
-    });
+    }, { paranoid: false });
 
     Document.associate = db => {
         db.Dog.hasMany(Document, { foreignKey: { allowNull: false, unique: "name" } });
