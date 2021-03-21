d53519bac3c574236b959bc645effc7fc15d3873
eubank87 <eubank.ashley@gmail.com>
2021-03-21 12:24:25 -0700

    set up use effect and set state for behavior assess answers by dog id. Started initial build of page

--- client/src/pages/BehavAssessAnswers.js
+++ client/src/pages/BehavAssessAnswers.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useParams} from "react-router-dom";
 import Grid from '@material-ui/core/Grid';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 
 import API from '../utils/API';
 
@@ -18,7 +21,9 @@ const useStyles=makeStyles(theme => ({
         },
         [theme.breakpoints.down("xs")]:{
             spacing: theme.spacing(2),
-            marginLeft: 0
+            marginLeft: 0,
+            paddingRight: 8,
+            paddingLeft: 8
         }
     }
 }))
@@ -27,9 +32,41 @@ const useStyles=makeStyles(theme => ({
 export default function BehaveAssessAnswers() {
     const classes = useStyles();
 
+    let {id} = useParams();
+    console.log(id)
+
+    const [behaveData, setBehaveData] = useState({
+        User: {},
+        response: {
+            ["Adult Men"]: 0,
+            ["Adult Women"]: 0,
+
+        }
+    })
+
+    useEffect(()=>{
+        API.getBehaviorAnswers(id).then(res =>{
+            console.log(res.data[0])
+            setBehaveData(res.data[0])
+        }).catch(err=>{
+            console.error(err)
+        })
+    },[])
+
     return (
         <Grid container className={classes.mainContainer}>
-            <h1>Behavior!</h1>
+            <Grid item style={{marginBottom: "1em"}}>
+                <Typography variant="h4" color="primary">Dog name's Behavior Assessment</Typography>
+                <Divider/>
+            </Grid>
+            <Grid item container>
+                <Grid item style={{marginRight: "4em"}}>
+                    <Typography style={{fontWeight: "bold"}}>Completed by: <Typography>{behaveData.User.firstName} {behaveData.User.lastName}</Typography></Typography>
+                </Grid>
+                <Grid item>
+                    <Typography style={{fontWeight: "bold"}}>Date: <Typography>{behaveData.date}</Typography></Typography>
+                </Grid>
+            </Grid>
         </Grid>
     )
 }
