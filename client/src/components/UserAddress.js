a330b2d2f5498b4988f11f4f1cd9c48ec7542a2d
eubank87 <eubank.ashley@gmail.com>
2021-03-28 09:45:04 -0700

    changed user address to card format

--- client/src/components/UserAddress.js
+++ client/src/components/UserAddress.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from "@material-ui/core/Grid";
-
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 
@@ -11,11 +14,26 @@ const useStyles = makeStyles((theme) => ({
             justifyContent: "center",
             alignItems: "center"
         }
+    },
+    root: {
+        minWidth: 275
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 3,
     }
 }));
 
 export default function UserAddress(props){
     const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
 
     return(
         <Grid item container className={classes.itemContainer}>
@@ -26,15 +44,31 @@ export default function UserAddress(props){
                     </Grid>
                     <Grid item container>
                         <Grid container>
-                            <Grid item container direction="column" style={{marginTop: "2em"}} xs={10} sm={10} md={6} lg={6}>
-                                <Typography style={{fontWeight: "bold"}}>Region:</Typography>
-                                <Typography style={{marginBottom: "1em"}}>{props.region}</Typography>
-                                <Typography style={{fontWeight: "bold"}}>Address:</Typography>
-                                <Typography name="street">{props.street}</Typography>
-                                <Typography name="street2">{props.street2}</Typography>
-                                <Typography name="city">{props.city}</Typography>
-                                <Typography name="state">{props.state}</Typography>
-                                <Typography name="zip5">{props.zip}</Typography>
+                            <Grid item container direction="column" style={{marginTop: "2em"}} xs={10} sm={10} md={2} lg={2}>
+
+                                <Card className={classes.root} variant="outlined">
+                                    <CardContent>
+                                        <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                            Region: 
+                                        </Typography>
+                                        <Typography variant="h6" component="h2">
+                                            {props.region}
+                                        </Typography>
+                                        <Typography className={classes.pos} color="textSecondary">
+                                            Address:
+                                        </Typography>
+                                        <Typography variant="body2" component="p">
+                                            {props.street}
+                                            <br />
+                                            {props.street2}
+                                            <br/>
+                                            {props.city}, {props.state} {props.zip}
+                                        </Typography>
+                                    </CardContent>
+                                    {/* <CardActions>
+                                        <Button size="small">Contact</Button>
+                                    </CardActions> */}
+                                </Card>
                             </Grid>
                         </Grid>
                     </Grid>
