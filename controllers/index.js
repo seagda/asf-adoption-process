da6ed989019d7686d2a2416d4f07ba69779f5359
JohnDJake <john.d.jake@gmail.com>
2021-03-29 17:08:50 -0700

    added app question and response controllers to index

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 module.exports = {
     alert: require("./alert"),
+    appQuestion: require("./appQuestion"),
+    appResponse: require("./appResponse"),
     document: require("./document"),
     dog: require("./dog"),
     user: require("./user")
