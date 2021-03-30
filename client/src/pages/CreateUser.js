9819623c6f1bbfd4398e23752df054f87a7fb355
JohnDJake <john.d.jake@gmail.com>
2021-03-30 15:02:18 -0700

    create user page uses edit profile page

--- client/src/pages/CreateUser.js
+++ client/src/pages/CreateUser.js
@@ -5,70 +5,19 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import ProfileForm from "../components/ProfileForm";
 import RoleAssignment from "../components/RoleAssignment";
 import API from '../utils/API';
-
-
-const useStyles=makeStyles(theme => ({
-    mainContainer: {
-        marginLeft: theme.spacing(35),
-        marginTop: theme.spacing(13),
-        width: "70%",
-        [theme.breakpoints.down("md")]:{
-            width: "80%"
-        },
-        [theme.breakpoints.down("sm")]:{
-            width: "100%"
-        },
-        [theme.breakpoints.down("xs")]:{
-            spacing: theme.spacing(2),
-            marginLeft: 0
-        }
-    }
-}))
-
+import EditProfile from './EditProfile';
 
 export default function CreateUser() {
-    const classes = useStyles();
-
-    const [userData, setUserData] = useState({})
-    const [addressData, setAddressData] = useState({})
-
-    const handleInputChange = event =>{
-        const {name,value} = event.target
-        setUserData({
-            ...userData,
-            [name]: value
-        })
-        setAddressData({
-            ...addressData,
-            [name]: value
-        })
-    }
-
-    const submitFunction = event =>{
-        event.preventDefault();
-        const newUser = {
-            ...userData,
-            ...addressData
-        }
-        API.createUser(newUser).then(res=>{
-            setUserData({})
-            setAddressData({})
-            window.location = `/`
-        }).catch(err=>{
+    const submitFunction = (userInputData, photoInput, redirect) => {
+        API.createUser(userInputData).then(res=>{
+            if (photoInput.type.startsWith("image")) return Promise.all([res.data.id, API.setProfilePhoto(photoInput, res.data.id)]);
+            return [res.data.id];
+        }).then(([id]) => redirect(`/user/${id}`)).catch(err=>{
             console.error(err.response.data.message)
-        })
+        });
     }
 
     return (
-        <Grid container className={classes.mainContainer}>
-            <ProfileForm 
-            editable={["*"]}
-            userData={userData} 
-            addressData={addressData} 
-            handleInputChange={handleInputChange} 
-            submitFunction={submitFunction}
-            />
-            {/* <RoleAssignment/> */}
-        </Grid>
+        <EditProfile userData={{ editable: ["*"] }} photo={new Blob()} submitFunction={submitFunction} />
     )
 }
