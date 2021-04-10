4ef5a7d4ade92d392dffd0164e721a959acf158b
JohnDJake <john.d.jake@gmail.com>
2021-04-09 18:41:01 -0700

    export getWithCoords

--- controllers/address.js
+++ controllers/address.js
@@ -3,16 +3,16 @@ const db = require("../models");
 const { Client } = require("@googlemaps/google-maps-services-js");
 const client = new Client();
 
-function getCoords(address) {
+module.exports.getWithCoords = address => {
     return client.geocode({ params: { address: Object.values((({ lat, long, ...address }) => address)(address)).join(","), key: process.env.GOOGLE_MAPS_API_KEY } })
         .then(res => ({ ...address, ...res.data.results[0].geometry.location }));
 }
 
 module.exports.create = address => {
-    return getCoords(address).then(withCoords => db.Address.create(withCoords));
+    return this.getWithCoords(address).then(withCoords => db.Address.create(withCoords));
 }
 
 module.exports.update = address => {
-    return Promise.all([db.Address.findByPk(address.id), getCoords(address)])
+    return Promise.all([db.Address.findByPk(address.id), this.getWithCoords(address)])
         .then(([Address, withCoords]) => Address.update(withCoords));
 }
