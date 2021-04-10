0e0e7195260c2097ffbd61831ee1643f17b82404
JohnDJake <john.d.jake@gmail.com>
2021-04-09 17:32:09 -0700

    long not lon
    standard abbreviation for clarity

--- models/Address.js
+++ models/Address.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
         city: { type: DataTypes.STRING, allowNull: false },
         zip5: { type: DataTypes.STRING, allowNull: false },
         lat: {type: DataTypes.DECIMAL(8,6), allowNull: false },
-        lon: {type: DataTypes.DECIMAL(9,6), allowNull: false}
+        long: {type: DataTypes.DECIMAL(9,6), allowNull: false}
     });
     return Address;
 };
