1b7ff3ad442059e2472b48e37bf1f655d0d0c6f4
Shea Mullaney <seagda@gmail.com>
2021-04-21 15:28:39 -0700

    adding date and cost to medistatus

--- models/MediStatus.js
+++ models/MediStatus.js
@@ -1,12 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
     const MediStatus = sequelize.define("MediStatus", {
         heartworm: { type: DataTypes.BOOLEAN, defaultValue: false },
+        hwRecent: DataTypes.DATEONLY,
+        hwExp: DataTypes.DATEONLY,
+        hwCost: DataTypes.INTEGER,
         hwPrevent: { type: DataTypes.BOOLEAN, defaultValue: false },
+        hwPrevRecent: DataTypes.DATEONLY,
+        hwPrevExp: DataTypes.DATEONLY,
+        hwPrevCost: DataTypes.INTEGER,
         distemper: { type: DataTypes.BOOLEAN, defaultValue: false },
+        distRecent: DataTypes.DATEONLY,
+        distExp: DataTypes.DATEONLY,
+        distCost: DataTypes.INTEGER,
         kennelCough: { type: DataTypes.BOOLEAN, defaultValue: false },
+        kcRecent: DataTypes.DATEONLY,
+        kcExp: DataTypes.DATEONLY,
+        kcCost: DataTypes.INTEGER,
         fourDX: { type: DataTypes.BOOLEAN, defaultValue: false },
+        fourDXRecent: DataTypes.DATEONLY,
+        fourDXExp: DataTypes.DATEONLY,
+        fourDXCost: DataTypes.INTEGER,
         fleaTick: { type: DataTypes.BOOLEAN, defaultValue: false },
-        altered: { type: DataTypes.BOOLEAN, defaultValue: false }
+        fleaTickRecent: DataTypes.DATEONLY,
+        fleaTickRecent: DataTypes.DATEONLY,
+        fleaTickCost: DataTypes.INTEGER,
+        altered: { type: DataTypes.BOOLEAN, defaultValue: false },
+        alteredDate: DataTypes.DATEONLY,
+        alteredCost: DataTypes.INTEGER
     });
 
     MediStatus.associate = db => {
